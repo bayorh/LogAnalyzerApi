da55a2269652a19c8651f8967a21975a212fd960
Toheeb BAYONLE <81241067+bayorh@users.noreply.github.com>
2024-05-09 10:30:05 +0100

    docker file edited with edited controller route

--- LogAnalyzerApi/Controllers/LogsController.cs
+++ LogAnalyzerApi/Controllers/LogsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LogAnalyzerApi.Controllers
 {
     [ApiController]
-    [Route("/")]
+    [Route("api/")]
     public class LogsController : ControllerBase
     {
         private readonly ILogService _logService;
@@ -14,48 +14,48 @@ namespace LogAnalyzerApi.Controllers
             _logService = logService;
         }
 
-        [HttpGet("/GetLogsBySize")]
+        [HttpGet("{path}/{minSizeInKByte}/{maxSizeinKByte}")]
         public IActionResult GetLogsBySize(string path, long minSizeInKByte, long maxSizeinKByte)
         {
             var result =_logService.SearchLogsSize(path, minSizeInKByte, maxSizeinKByte);
             return Ok(result);
         }
-        [HttpGet("/SearchLogsInDirectories")]
-        public IActionResult SearchLogsInDirectories(string[] directoriespaths)
+        [HttpGet("{directoriespaths}")]
+        public IActionResult SearchLogsInDirectories([FromQuery] string[] directoriespaths)
         {
             var result = _logService.SearchLogsInDirectories(directoriespaths);
             return Ok(result);
         }
 
-        [HttpGet("/SearchLogsInADirectory")]
+        [HttpGet("SearchLogsInADirectory{path}")]
         public IActionResult SearchLogsInADirectory(string path)
         {
             var result = _logService.SearchLogsInADirectory(path);
             return Ok(result);
         }
-        [HttpGet("/GetLogsCountsInADirectoryForAPeriod")]
+        [HttpGet("{directoryPath}/{startDate}/{endDate}")]
         public IActionResult GetLogsCountsInADirectoryForAPeriod(string directoryPath, DateTime startDate, DateTime endDate)
         {
-            var result = _logService.GetLogsCountsInADirectoryForAPeriod(directoryPath,startDate,endDate);
+            var result = _logService.GetLogsCountsInADirectoryForAPeriod(directoryPath, startDate, endDate);
             return Ok(result);
         }
 
-        [HttpDelete("/DeleteArchive")]
+        [HttpDelete("DeleteArchive{targetDirectory}/{startDate}/{endDate}")]
         public IActionResult DeleteArchive(string targetDirectory, DateTime startDate, DateTime endDate)
-        { 
+        {
             var result = _logService.DeleteArchive(targetDirectory, startDate, endDate);
             return Ok(result);
         }
 
-        [HttpDelete("/DeleteAllLogsInAPeriod")]
+        [HttpDelete("{targetDirectory}/{startDate}/{endDate}")]
         public IActionResult DeleteAllLogsInAPeriod(string targetDirectory, DateTime startDate, DateTime endDate)
         {
             var result = _logService.DeleteAllLogsInAPeriod(targetDirectory, startDate, endDate);
             return Ok(result);
         }
 
-        [HttpPost("/ArchiveLogFiles")]
-        public IActionResult ArchiveLogFiles(string sourceDirectory, 
+        [HttpPost("{sourceDirectory}/{targetDirectory}/{startDate}/{endDate}")]
+        public IActionResult ArchiveLogFiles(string sourceDirectory,
             string targetDirectory, DateTime startDate, DateTime endDate)
         {
             var result = _logService.ArchiveLogFiles(sourceDirectory, targetDirectory, startDate, endDate);
